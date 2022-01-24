a4b7e8a14542d5864f754509d30622b53f460b35
unknown <uriasafranciscojr@gmail.com>
2022-01-24 13:11:45 -0300

    captar numeros

--- Project_Calculator/script.js
+++ Project_Calculator/script.js
@@ -1,8 +1,31 @@
 var number1 = "";
 var number2 = "";
+var operator = undefined;
+
 
 function getNumber(number) {
-    number1 = number1 + number;
-    console.log("number ", number1);
+
+    
+    if (operator === undefined) {
+        number1 = number1 + number;
+        showNumber(number1)
+    } else {
+        number2 = number2 + number;
+        showNumber(number2)
+    }
+    console.log("number1", number1);
+    console.log("operator", operator);
+    console.log("number2", number2);
+    
+}
+
+function getOperation(operation) {
+    operator = operation;
 
 }
+
+function showNumber(number) {
+    let screen = document.getElementById("screen");
+    screen.innerHTML = "<p>" + number + "</p>";
+}
+
